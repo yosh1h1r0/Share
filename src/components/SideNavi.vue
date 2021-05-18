34e61776a9516271f294ceb0e1631acfd11e9132
yoshihiro <yossy8931@gmail.com>
2021-05-18 17:37:30 +0900

    first-commit

---
+++ src/components/SideNavi.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+      <div class="left-padding">
+          <img class="logo" src="../assets/logo.png" >
+          <div class="flex icon-field" @click="$router.push('/home')">
+            <img class="icon" src="../assets/home.png" >
+            <p>ホーム</p>
+          </div>
+            <div class="flex icon-field" @click="$router.push('/profile')">
+            <img class="icon" src="../assets/profile.png" >
+            <p>プロフィール</p>
+          </div>
+            <div class="flex icon-field" @click="$router.push('/logout')">
+            <img class="icon" src="../assets/logout.png" >
+            <p>ログアウト</p>
+          </div>
+      </div>
+      <ShareMessage/>
+  </div>
+</template>
+
+<script>
+import ShareMessage from "../components/ShareMessage";
+export default {
+components: {
+    ShareMessage
+}
+};
+</script>
+
+<style scoped>
+.left-padding {
+    margin: 20px;
+}
+.logo {
+    width: 100%;
+}
+.icon-field {
+    margin-top:15px;
+    cursor: pointer;
+}
+.icon {
+    width: 25px;
+}
+.icon-field p {
+    font-size: 16px;
+    padding-left: 15px;
+}
+</style>
