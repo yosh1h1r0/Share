73d45fd5142d9e8c3df732db29957c7803d0b061
yoshihiro <yossy8931@gmail.com>
2021-05-23 17:03:10 +0900

    eslintrc-rules-on

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,12 +11,7 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'on',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  },
-"eslintConfig":{
-	"rules":{
-		"no-console":"warn"
-	}
-}
+  }
 }
